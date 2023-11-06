8f2b8cd73e2a392ce60138b25bafc79bae81fcee
Jonathan Blau <122023139+CoolSandwich20@users.noreply.github.com>
2023-11-06 19:07:34 +0200

    First Movement

--- Assets/Scripts/Player_Handler.cs
+++ Assets/Scripts/Player_Handler.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player_Handler : MonoBehaviour
 {
+    [SerializeField] GameObject Guy;
+    [SerializeField] GameObject Gal;
+    [SerializeField] Rigidbody2D CoupleRB;
+    [SerializeField] float MoveSpeed = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveCouple(true);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveCouple(false);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            MoveSpeed = 10;
+        }               
+        else
+        {
+            MoveSpeed = 5;
+        }
+
     }
+
+
+    void MoveCouple(bool isLeft = false)
+    {
+
+        if (!isLeft) CoupleRB.transform.Translate(Vector2.right * MoveSpeed * Time.deltaTime);
+        if (isLeft) CoupleRB.transform.Translate(Vector2.left * MoveSpeed * Time.deltaTime);
+
+    }
+    
 }
